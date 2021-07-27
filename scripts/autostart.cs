b4df86b0940b6e1731776d17dc0dd9e98b815f6e
Damian Diaz <a@b.c>
2021-07-27 14:58:27 +0200

    Autostart added

---
+++ scripts/autostart.cs
@@ -0,0 +1,11 @@
+
+#  To set this as autostart script, type in the inputline:
+#    =>script_autostart_set('./scripts/autostart.cs') 
+#
+
+output_str( '\n\nautostart.cs: running startup...\n' )
+
+set_line_end('\r')
+set_serial_mode(3,'\r')
+
+output_str( '\n\nautostart.cs: startup done.\n' )
